80a08e727dc180f01c8c18e5e6fcb3eea524c4fb
Adam Kramer <akramer@gmail.com>
2019-08-08 22:15:33 +0000

    Now using httputil/ReverseProxy, which seems better than trying to write my own.

--- razorwire.go
+++ razorwire.go
@@ -16,6 +16,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"regexp"
 	"strings"
@@ -48,7 +49,7 @@ var args struct {
 
 type proxy struct {
 	backendURLMap map[string]*url.URL
-	clientMap     map[string]*http.Client
+	proxyMap      map[string]*httputil.ReverseProxy
 }
 
 var proxyRegexp = regexp.MustCompile("^(http|https|nohttps)-([0-9]+)-([0-9]+)-([0-9]+)-([0-9]+)(?:-([0-9]+))?$")
@@ -63,7 +64,7 @@ func newClient() *http.Client {
 func newProxy() proxy {
 	p := proxy{}
 	p.backendURLMap = make(map[string]*url.URL)
-	p.clientMap = make(map[string]*http.Client)
+	p.proxyMap = make(map[string]*httputil.ReverseProxy)
 	for _, n := range args.Proxy {
 		vals := strings.Split(n, ",")
 		if len(vals) != 2 {
@@ -80,6 +81,36 @@ func newProxy() proxy {
 	return p
 }
 
+func newReverseProxy(backend *url.URL, validateCert bool) *httputil.ReverseProxy {
+	rp := &httputil.ReverseProxy{}
+	rp.ModifyResponse = func(resp *http.Response) error {
+		resp.Header.Set("Strict-Transport-Security", "max-age=31536000;")
+		return nil
+	}
+	rp.Director = func(req *http.Request) {
+		req.Header.Set("X-Forwarded-Host", req.Host)
+		req.Header.Set("X-Forwarded-Proto", "https")
+		req.Header.Del("Authorization")
+		req.Header.Del("Close")
+		req.URL.Scheme = backend.Scheme
+		req.URL.Host = backend.Host
+		req.Host = backend.Host
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+		fmt.Printf("Proxying request %+v\n", req)
+	}
+	if !validateCert {
+		rp.Transport = &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		}
+	}
+	return rp
+}
+
 func (p proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	user, password, ok := r.BasicAuth()
 	if !ok {
@@ -93,27 +124,15 @@ func (p proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	s := strings.Split(r.Host, ".")
-	cl, ok := p.clientMap[s[0]]
-	if !ok {
-		fmt.Printf("Creating new http.Client for %s\n", s[0])
-		cl = newClient()
-		p.clientMap[s[0]] = cl
-	}
-	u, ok := p.backendURLMap[s[0]]
 	var err error
+	validate := true
+	u, ok := p.backendURLMap[s[0]]
 	if !ok {
 		if sm := proxyRegexp.FindStringSubmatch(s[0]); sm != nil {
 			var ur string
 			if sm[1] == "nohttps" {
 				sm[1] = "https"
-				if cl.Transport == nil {
-					t := http.Transport{
-						TLSClientConfig: &tls.Config{
-							InsecureSkipVerify: true,
-						},
-					}
-					cl.Transport = &t
-				}
+				validate = false
 			}
 			if len(sm) < 7 || sm[6] == "" {
 				ur = fmt.Sprintf("%s://%s.%s.%s.%s/", sm[1], sm[2], sm[3], sm[4], sm[5])
@@ -123,109 +142,44 @@ func (p proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			u, err = url.Parse(ur)
 			if err != nil {
 				http.Error(w, fmt.Sprintf("unknown host: %s", s[0]), 404)
+				return
 			}
 		} else {
 			http.Error(w, fmt.Sprintf("unknown host: %s", s[0]), 404)
 			return
 		}
 	}
-	(&proxyRequest{
-		client:     cl,
-		backendURL: u,
-	}).ServeHTTP(w, r)
+	pr, ok := p.proxyMap[s[0]]
+	if !ok {
+		fmt.Printf("Creating new httputil.ReverseProxy for %s, url: %+v\n", s[0], u)
+		pr = newReverseProxy(u, validate)
+		p.proxyMap[s[0]] = pr
+	}
+	pr.ServeHTTP(w, r)
 }
 
-type proxyRequest struct {
-	client     *http.Client
-	backendURL *url.URL
-}
-
-func (p *proxyRequest) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	header := make(http.Header)
-	for k, v := range r.Header {
-		// should probably be case insensitive matches
-		if k == "Authorization" || k == "Close" || k == "Host" || k == "Accept-Encoding" {
-			continue
+func checkRedirect(req *http.Request, resp *http.Response) (*url.URL, error) {
+	/*	loc, found := resp.Header["Location"]
+		if !found {
+			return nil, fmt.Errorf("Location header not found in redirect request")
 		}
-		fmt.Printf("Adding header %s: %v\n", k, v)
-		header[k] = v
-	}
-	u, err := url.Parse(fmt.Sprintf("%s://%s%s", p.backendURL.Scheme, p.backendURL.Host, r.RequestURI))
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	req := http.Request{
-		URL:           u,
-		Method:        r.Method,
-		Header:        header,
-		Body:          r.Body,
-		ContentLength: r.ContentLength,
-		Trailer:       r.Trailer,
-	}
-	fmt.Printf("Sending a request to %+v\n", req)
-	resp, err := p.client.Do(&req)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	fmt.Printf("Got response code %d\n", resp.StatusCode)
-	var redirectURL *url.URL
-	if resp.StatusCode == 301 || resp.StatusCode == 302 {
-		fmt.Printf("Running redirect check\n")
-		redirectURL, err = p.checkRedirect(r, resp)
+		fmt.Printf("Found location header %s\n", loc[0])
+		u, err := url.Parse(loc[0])
 		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-	}
-	for k, v := range resp.Header {
-		if k == "Connection" {
-			continue
-		}
-		if redirectURL != nil {
-			if k == "Location" {
-				w.Header().Set("Location", redirectURL.String())
-			}
-			continue
-		}
-		for _, vs := range v {
-			w.Header().Add(k, vs)
-		}
-		w.Header().Set("Strict-Transport-Security", "max-age=31536000;")
-		w.Header().Set("X-Forwarded-Proto", "https")
-		// TODO: this should comma append, not set a second x-forwarded-for header
-		w.Header().Add("X-Forwarded-For", r.RemoteAddr)
-		if w.Header().Get("X-Forwarded-Host") == "" {
-			w.Header().Set("X-Forwarded-Host", r.Host)
-		}
-	}
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
-}
-
-func (p *proxyRequest) checkRedirect(req *http.Request, resp *http.Response) (*url.URL, error) {
-	loc, found := resp.Header["Location"]
-	if !found {
-		return nil, fmt.Errorf("Location header not found in redirect request")
-	}
-	fmt.Printf("Found location header %s\n", loc[0])
-	u, err := url.Parse(loc[0])
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing URL in redirect request, %v", err)
-	}
+			return nil, fmt.Errorf("Error parsing URL in redirect request, %v", err)
+		}*/
 	// If the 301 or 302 points at the same host/port/scheme that we just talked to, redirect to the same.
 	// TODO: if the redirect points at another proxyable resource besides the origin, rewrite that too.
-	fmt.Printf("%s %s %s vs %s %s %s", u.Hostname(), u.Port(), u.Scheme, p.backendURL.Hostname(), p.backendURL.Port(), p.backendURL.Scheme)
-	if u.Hostname() == p.backendURL.Hostname() && u.Port() == p.backendURL.Port() && u.Scheme == p.backendURL.Scheme {
-		n, err := url.Parse(fmt.Sprintf("%s://%s/%s", req.URL.Scheme, req.URL.Host, u.RequestURI()))
-		if err != nil {
-			return nil, fmt.Errorf("Error parsing generated URL for redirect: %v", err)
+	//fmt.Printf("%s %s %s vs %s %s %s", u.Hostname(), u.Port(), u.Scheme, p.backendURL.Hostname(), p.backendURL.Port(), p.backendURL.Scheme)
+	/*	if u.Hostname() == p.backendURL.Hostname() && u.Port() == p.backendURL.Port() && u.Scheme == p.backendURL.Scheme {
+			n, err := url.Parse(fmt.Sprintf("%s://%s/%s", req.URL.Scheme, req.URL.Host, u.RequestURI()))
+			if err != nil {
+				return nil, fmt.Errorf("Error parsing generated URL for redirect: %v", err)
+			}
+			fmt.Printf("Location matched - patching redirect to %s\n", n.String())
+			return n, nil
 		}
-		fmt.Printf("Location matched - patching redirect to %s\n", n.String())
-		return n, nil
-	}
-	fmt.Printf("Location didn't match, not rewriting redirect\n")
+		fmt.Printf("Location didn't match, not rewriting redirect\n")*/
 	return nil, nil
 }
 
